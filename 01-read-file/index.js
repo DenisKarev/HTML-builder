55977cff83c22be16dd0725d7ce74125627c7d18
Denis.Karev <goofy.adun@gmail.com>
2025-01-18 23:53:07 +0300

    feat: add read file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const fileName = 'text.txt';
+const file = path.join(__dirname, fileName);
+
+const rs = fs.createReadStream(file);
+rs.on('data', (stream) => {
+    const res = process.stdout.write(stream);
+});
