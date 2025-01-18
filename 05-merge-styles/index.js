62ccceb591e3abb5a196082ac62ecb6fe82163ce
Denis.Karev <goofy.adun@gmail.com>
2025-01-19 02:35:11 +0300

    feat: add css bundling solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const path = require('node:path');
+// const fsp = require('node:fs/promises');
+const fs = require('node:fs');
+
+const source = 'styles';
+const project = 'project-dist';
+const bundleFN = 'bundle.css';
+const sourcePath = path.join(__dirname, source);
+const projectPath = path.join(__dirname, project);
+
+cssBundle(sourcePath, projectPath, bundleFN);
+
+async function cssBundle(source, dest, file) {
+  const bundle = path.join(dest, file);
+  let sourceFiles, bundleStream;
+  try {
+    bundleStream = fs.createWriteStream(bundle);
+    fs.readdir(source, { withFileTypes: true }, (err, files) => {
+      if (!err) {
+        files.forEach((file) => {
+          const fileFullPath = path.join(sourcePath, file.name);
+          if (path.extname(fileFullPath) === '.css') {
+            const rs = fs.createReadStream(fileFullPath);
+            rs.setEncoding('utf-8');
+            // rs.on('data', (chunk) => {
+            //   bundleStream.write(chunk);
+            // })
+            rs.pipe(bundleStream);
+          }
+        })
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
