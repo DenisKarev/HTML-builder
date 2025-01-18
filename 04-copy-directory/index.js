6de147746eac1999c3985d591d454ac2710e03a0
Denis.Karev <goofy.adun@gmail.com>
2025-01-19 00:59:39 +0300

    feat: add copy files solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,75 +1,45 @@
 const path = require('node:path');
-// const fs = require('node:fs');
 const fsp = require('node:fs/promises');
-// const os = require("os");
 
 const source = 'files';
 const destination = 'files-copy';
 const sourcePath = path.join(__dirname, source);
 const destPath = path.join(__dirname, destination);
 
-
 copyFiles(sourcePath, destPath).catch(err => console.log(err));
 
-// makeDirectory(targetPath).catch(err => console.log(err));
-
 async function copyFiles(source, dest) {
-    const sourceFiles = await fsp.readdir(dirPath, { withFileTypes: true });
-    const dir = await fsp.mkdir(dest, { recursive: true });
-    await makeDirectory(dest).catch(err => console.log(err));
-    // console.log(sourceFiles);
-    // console.log(sourcePath, destPath);
+    let sourceFiles, dir, destFiles;
+    try {
+        sourceFiles = await fsp.readdir(sourcePath, { withFileTypes: true });
+        dir = await fsp.mkdir(destPath, { recursive: true });
+        if (dir == undefined) {
+            destFiles = await fsp.readdir(destPath, { withFileTypes: true });
+        }
+    } catch (error) {
+        console.log(error);
+    }
     sourceFiles.forEach(async (file) => {
         const srcFilePath = path.join(source, file.name)
         const destFilePath = path.join(dest, file.name)
         const stat = await fsp.stat(srcFilePath);
         if (stat.isFile()) {
-            await fsp.copyFile(srcFilePath, destFilePath);
-        } else if (stat.isDirectory()) {
-            const newSrc = path.join(source, file.name);
-            const newDest = path.join(dest, file.name);
-            console.log(newSrc, '|', newDest);
-            await copyFiles(newSrc, newDest);
+            try {
+                await fsp.copyFile(srcFilePath, destFilePath);
+            } catch (error) {
+                console.log(error);
+            }
         }
     })
-}
-
-async function makeDirectory(path) {
-    let dir;
-    try {
-        dir = await fsp.mkdir(path, { recursive: true });
-        if (!dir) dir = 'err';
-    } catch (error) {
-        // console.log(error);
-        dir = 'err';
-    }
-    if (dir === 'err') {
-        const stat = await fsp.stat(path);
-        if (stat.isFile()) {
-            console.log(`${path} file exist. Deleting... and recreating directory`);
-            const err = await fsp.rm(path, { recursive: true });
-            if (!err) await makeDirectory(path);
-        } else if (stat.isDirectory()) {
-            const files = await readFilesInDir(destPath);
-            if (files?.length) {
-                const err = await fsp.rm(path, { recursive: true });
-                if (!err) {
-                    fsp.mkdir(path, { recursive: true })
-                    console.log(`Non-empty directory ${path} already exist.\nDeleted and recreated. (I don't bother had it files or not, i need empty directory! ))`);
-                }
+    destFiles.forEach(async (file) => {
+        const destFilePath = path.join(dest, file.name)
+        const stat = await fsp.stat(destFilePath);
+        if (stat.isFile() && sourceFiles.findIndex((e) => e.name === file.name) === -1) {
+            try {
+                await fsp.rm(destFilePath)
+            } catch (error) {
+                console.log(error);
             }
-        } else {
-            console.log(error);
         }
-    }
-}
-
-async function readFilesInDir(dirPath) {
-    let allFiles;
-    try {
-        allFiles = await fsp.readdir(dirPath, { withFileTypes: true });
-        return allFiles;
-    } catch (error) {
-        console.log(error);
-    }
+    })
 }
