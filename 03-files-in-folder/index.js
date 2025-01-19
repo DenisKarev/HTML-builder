2b9b1fd010b60055e62084ba765c61f901c4ebf0
Denis.Karev <goofy.adun@gmail.com>
2025-01-19 22:25:16 +0300

    refactor: add refactoring (according to linter/prettier)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,23 @@
 const path = require('node:path');
-const fs = require('node:fs');
+// const fs = require('node:fs');
 const fsp = require('node:fs/promises');
 
 const dirPath = 'secret-folder';
 const fullPath = path.join(__dirname, dirPath);
 
 // Async readdir
-fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.join(fullPath, file.name);
-      fs.stat(filePath, (err, stat) => {
-        const fileExt = path.extname(file.name);
-        const fileName = path.basename(filePath, fileExt);
-        console.log(`${fileName} - ${fileExt.slice(1)} - ${stat.size}b`);
-      });
-    }
-  });
-});
+// fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
+//   files.forEach((file) => {
+//     if (file.isFile()) {
+//       const filePath = path.join(fullPath, file.name);
+//       fs.stat(filePath, (err, stat) => {
+//         const fileExt = path.extname(file.name);
+//         const fileName = path.basename(filePath, fileExt);
+//         console.log(`${fileName} - ${fileExt.slice(1)} - ${stat.size}b`);
+//       });
+//     }
+//   });
+// });
 
 // Promises readdir
 readFilesInDir(fullPath).then((files) => {
@@ -27,9 +27,8 @@ readFilesInDir(fullPath).then((files) => {
       if (st.isFile()) {
         const fileExt = path.extname(file.name);
         const fileName = path.basename(file.name, fileExt);
-        console.log(`
-${fileName} - ${fileExt.slice(1)} - ${(st.size / 1024).toFixed(2)}Kb
-`);
+        const fileSize = (st.size / 1024).toFixed(2);
+        console.log(`${fileName} - ${fileExt.slice(1)} - ${fileSize}Kb`);
       }
     });
   } else {
